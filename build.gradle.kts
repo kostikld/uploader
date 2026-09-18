c1577dca2ea90d6b4a0370d0962e8ebe1261ab7c
kavo1018 <kanstantsin.voitau@netcracker.com>
2026-09-18 14:16:00 +0300

    support IDEA since 2023.3.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        intellijIdea("2026.2.0.1") {
+        intellijIdea("2023.3.3") {
             useInstaller = false
         }
         jetbrainsRuntime()
